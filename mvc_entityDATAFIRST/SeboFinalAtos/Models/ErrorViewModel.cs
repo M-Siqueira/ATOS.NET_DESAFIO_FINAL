a2bb628b805d95fe73e1b673581b61cac5327762
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-12-15 15:15:57 -0300

    recomecoProjetoFin2

---
+++ mvc_entityDATAFIRST/SeboFinalAtos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeboFinalAtos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
