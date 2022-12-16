da5bf73b370e0584430a1685ac9c032972827417
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-12-16 12:59:33 -0300

    HtmlEditProjetoFin2

--- mvc_entityDATAFIRST/SeboFinalAtos/Controllers/HomeController.cs
+++ mvc_entityDATAFIRST/SeboFinalAtos/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SeboFinalAtos.Controllers
             return View();
         }
 
+        public IActionResult Pdv()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
